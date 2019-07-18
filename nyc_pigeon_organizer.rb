cbaf0eb3555019cce9743eb5ecab5c32e79f8b32
Barrette Banner <barrettebanner@gmail.com>
2019-07-18 21:56:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final = {}
+  data.each do |first, others|
+    others.each do |category, array|
+      array.each do |name|
+        final[name] = {color: [], gender: [], lives: []}
+      end
+    end
+  end 
+  x = final.keys
+  data[:color].each do |color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << color.to_s 
+        end
+      end
+    end
+  end 
 end
